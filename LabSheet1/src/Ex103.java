2f37e1d66782df761133391a037719d4918fd2df
Noah-Supharat <noahsuphanan148@gmail.com>
2025-11-18 13:41:54 +0700

    submit exercise LabSheet01

---
+++ LabSheet1/src/Ex103.java
@@ -0,0 +1,10 @@
+import javax.swing.*;
+
+public class Ex103 {
+    public static void main(String[] args) {
+        JOptionPane.showMessageDialog(null,
+                "I am Suphanan Supharat" +
+                        "\nI want grade A",
+                "Message", JOptionPane.WARNING_MESSAGE);
+    }
+}
